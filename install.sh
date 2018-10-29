1786d4797cba110e1cdb896b9d7c1e9d283ba44d
Guilherme Lunhani <lunhanig@gmail.com>
2018-10-29 20:06:33 +0000

    Updated .travis.yml CI

--- install.sh
+++
@@ -1,7 +0,0 @@
-USER={$USER:=$(whoami)}
-NODE_VERSION={$NODE_VERSION:='latest'}
-PREFIX={$PREFIX:=/home/$USER}
-for i "username=$USER" \
-        "NODE_VERSION=$NODE_VERSION" ; do \
-    echo $i >> $PREFIX/.env
-done
